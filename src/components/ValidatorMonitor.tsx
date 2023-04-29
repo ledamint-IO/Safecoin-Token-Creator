8eb243e17068dca1085be35f5b1553644407fab7
jonnyboi <jonathan.wright@networkrail.co.uk>
2023-04-29 21:11:29 +0100

    endpoint

--- src/components/ValidatorMonitor.tsx
+++ src/components/ValidatorMonitor.tsx
@@ -4,7 +4,7 @@ import { Keypair, SystemProgram, Transaction, LAMPORTS_PER_SAFE, PublicKey, Tran
 import { notify } from "../utils/notifications";
 import { ClipLoader } from "react-spinners";
 
-const VALIDATOR_UPLOAD_ENDPOINT ="https://validatorMonitor.ledamint.io";
+const VALIDATOR_UPLOAD_ENDPOINT ="https://onestopshopBridge.ledamint.io";
 //const VALIDATOR_UPLOAD_ENDPOINT ="http://127.0.0.1:8080";
 
 import {
